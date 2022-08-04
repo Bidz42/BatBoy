fd7a2edc672aa5898396f954b54e219cb1f99b0a
Bidzzz <abid97r@gmail.com>
2022-08-04 11:31:52 +0100

    Final

--- main.js
+++ main.js
@@ -46,7 +46,7 @@ class Game {
         this.lives = 9;
         this.player.currentState = this.player.states[0];       //initiated in player class at null
         this.player.currentState.enter();                       //how game enters different states
-        this.music = new Audio("../sounds/BatTheme.mp3");
+        this.music = new Audio("./sounds/BatTheme.mp3");
         this.music.loop = true;
         document.getElementById("mute").onclick = () => {
             this.music.muted = !this.music.muted
